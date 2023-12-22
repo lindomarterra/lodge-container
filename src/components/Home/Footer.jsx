bda1c19d73d78a0bbc409c793f979f974565c9b7
Terra <terra.lindomar@gmail.com>
2023-12-22 17:54:52 -0300

    dez 22 2023 18:00

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -1,55 +1,55 @@
 import React from 'react' 
-import { MdOutlineStarBorder } from "react-icons/md"
+import '../../index.css'
+import { IoMdStar } from "react-icons/io"
 import { BsFillTelephoneFill } from "react-icons/bs"
 import { MdOutlineEmail } from "react-icons/md"
 import { FaLocationDot } from "react-icons/fa6"
 
 function Footer() {
     return ( 
-        <footer className='p-2' >
+        <footer style={{backgroundColor:'whitesmoke'}} className='py-5' >
             <div className='container-md' >
                 <div className="row">
                     
-                    <div className="col-md-6 col-md-4 col-lg-3">
-                        <div className="d-flex flex-column justify-content-between p-2">
-                            <div className='divTop'>
-                                <small className='fw-bold' > POUSADA VILLA CONTAINER LODGE </small>
-                                <small className='text-black-50 '> CNPJ: 31.707.370/0001-97 </small>
-                                <div>
-                                    <MdOutlineStarBorder className='fs-3 bg-danger '  />
-                                    <MdOutlineStarBorder className='fs-3  '  />
-                                </div>
-                            </div>        
-                            <div className='divBottom'>
-                                <BsFillTelephoneFill />
-                                <small> Whatsapp:  +55 (03) 7 9992-5234 </small>
-                            </div>  
+                    <div className="col-md-6 col-lg-4">
+                        <div className="p-2" >
+                            <small className='fw-bold' > POUSADA VILLA CONTAINER LODGE </small> 
+                            <small className='text-black-50'> CNPJ:31.707.370/0001-97 </small>
+                            <div>
+                                <IoMdStar className='text-warning fs-5' />
+                                <IoMdStar className='text-warning fs-5' />
+                                <IoMdStar className='text-warning fs-5' />
+                                <IoMdStar className='text-warning fs-5' />
+                                <IoMdStar className='text-black-50 fs-5 opacity-25' />
+                            </div>                            
                         </div>
                     </div>
 
-                    <div className="col-md-6 col-md-4 col-lg-3">
-                        <div className="d-flex justify-content-center align-items-center p-2">
-                                <p>reservas@villacontainerlodge.com.br</p>
-                                <MdOutlineEmail />
+                    <div className="col-md-6 col-lg-4">
+                        <div className="p-2">
+                            <p>
+                                <span > <MdOutlineEmail className='fs-4 me-1 text-black-50' /></span>
+                                <small className='pt-1 text-black-50 reservasHover' >reservas@villacontainerlodge.com.br</small>
+                            </p>
+                            <p>
+                                <BsFillTelephoneFill className='fs-5 me-1 text-black-50' />
+                                <small className='ms-2 zapHover text-black-50' > Whatsapp:  +55 (03) 7 9992-5234 </small>
+                            </p>
                         </div>
                     </div>
 
-                    <div className="col-md-6 col-md-4 col-lg-3">
+                    <div className="col-md-6 col-lg-4">
                         <div className="d-flex justify-content-center align-items-center p-2">
-                            <FaLocationDot />            
-                            <div className='divEndereco' >
-                                <p> Estrada Macaúbas, Km1.5, Centro </p>
-                                <p>Capitólio, MinasGerais, Brasil</p>
-                                <p>CEP: 37.930-000</p>
+                            <FaLocationDot className='me-2 fs-3 text-black-50' />            
+                            <div className='text-black-50 enderecoHover ' >
+                                <small> Estrada Macaúbas, Km 1.5, Centro </small> <br/>
+                                <small>Capitólio, Minas Gerais, Brasil</small> <br/>
+                                <small>CEP: 37.930-000</small>
                             </div>
                         </div>
                     </div>
 
-                    <div className="col-md-6 col-md-4 col-lg-3">
-                        <div className="d-flex justify-content-center align-items-center p-2">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati dolorem impedit quod nam nisi veniam, animi earum eveniet cumque commodi officia molestias. Voluptatem eius molestias est dolore ipsum id doloremque.
-                        </div>
-                    </div>
+                    
 
 
                 </div>
--- src/components/Home/UnderFooter.jsx
+++ src/components/Home/UnderFooter.jsx
@@ -1,12 +1,18 @@
 import React from 'react' 
+import { CgInstagram } from "react-icons/cg"
+import { TfiYoutube } from "react-icons/tfi"
+import { SiTripadvisor } from "react-icons/si"
+import '../../index.css'
 
 function UnderFooter() {
     return ( 
-        <div style={{backgroundColor:'grey'}} className='p-2 mb-5'>
-        
-            <p className='py-5 text-white-50 text-center' >UNDERFOOTER</p>        
-        
-        </div>
+        <section style={{backgroundColor:'snow'}} className='mt-1 mb-5 pb-5'>
+            <div className='p-5 text-center'>
+                <span className='m-1 p-4 rounded-2 instaHover'><CgInstagram   className='fs-4 text-black '/></span>
+                <span className='m-1 p-4 rounded-2 youTubeHover'><TfiYoutube  className='fs-4 text-black '/></span>
+                <span className='m-1 p-4 rounded-2 tripHover'><SiTripadvisor  className='fs-4 text-black '/></span>            
+            </div>
+        </section>
     )
 }
 
